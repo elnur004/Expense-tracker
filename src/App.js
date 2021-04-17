0f6a94c4874e788545477c645c6bb6e06214d0b7
elnur004 <lnur004@gmail.com>
2021-04-18 02:03:26 +0400

    Created initial component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-function App() {
+import ExpenseItem from './components/ExpenseItem';
+
+const App = () => {
   return (
     <div>
       <h2>Let's get started!</h2>
+      <ExpenseItem />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+const ExpenseItem = () => {
+  return (
+    <div>
+      <div>April 18nth</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>$294.7</div>
+      </div>
+    </div>
+  );
+};
+
+export default ExpenseItem;
