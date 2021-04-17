18e7a236bc0bc05a2de775c023775f1670b71e8a
elnur004 <lnur004@gmail.com>
2021-04-18 02:16:55 +0400

    Added some style to component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,12 @@
+import './ExpenseItem.css';
+
 const ExpenseItem = () => {
   return (
-    <div>
+    <div className="expense-item">
       <div>April 18nth</div>
-      <div>
+      <div className="expense-item__description">
         <h2>Car Insurance</h2>
-        <div>$294.7</div>
+        <div className="expense-item__price">$294.7</div>
       </div>
     </div>
   );
