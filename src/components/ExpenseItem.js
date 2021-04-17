cd87df374e5ef8d8c24129462774a05e36b98056
elnur004 <lnur004@gmail.com>
2021-04-18 02:38:45 +0400

    added dynamically output data

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
 import './ExpenseItem.css';
 
 const ExpenseItem = () => {
+  const expenseDate = new Date(2021, 3, 18);
+  const expenseTitle = 'Car Insurance';
+  const expenseAmount = 294.7;
+
   return (
     <div className="expense-item">
-      <div>April 18nth</div>
+      <div>{expenseDate.toLocaleString()}</div>
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$294.7</div>
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
       </div>
     </div>
   );
