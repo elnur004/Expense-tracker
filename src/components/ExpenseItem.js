5b9eb172294eee120cb32d8554ef37467d970c51
elnur004 <lnur004@gmail.com>
2021-04-18 04:49:14 +0400

    getting dynamically output by props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,12 @@
 import './ExpenseItem.css';
 
-const ExpenseItem = () => {
-  const expenseDate = new Date(2021, 3, 18);
-  const expenseTitle = 'Car Insurance';
-  const expenseAmount = 294.7;
-
+const ExpenseItem = (props) => {
   return (
     <div className="expense-item">
-      <div>{expenseDate.toLocaleString()}</div>
+      <div>{props.date.toLocaleString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
