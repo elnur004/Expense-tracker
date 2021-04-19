a38c4ebb09f3be3a025616caa1f88fd4f139d0c7
elnur004 <lnur004@gmail.com>
2021-04-20 01:00:54 +0400

    cleaned the date structure

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const App = () => {
       id: 'e1',
       title: 'Toilet Paper',
       amount: 94.12,
-      date: new Date(2020, 7, 14),
+      date: new Date(2020, 7, 4),
     },
     { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
     {
@@ -19,7 +19,7 @@ const App = () => {
       id: 'e4',
       title: 'New Desk (Wooden)',
       amount: 450,
-      date: new Date(2021, 5, 12),
+      date: new Date(2021, 5, 2),
     },
   ];
 
