ab0318c6411d7df45ce1af6ac822467ff577f510
elnur004 <lnur004@gmail.com>
2021-04-29 03:48:08 +0400

    added indicating expense values on the chart bar dynamically

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const DUMMY_EXPENSES = [
     amount: 94.12,
     date: new Date(2020, 7, 4),
   },
-  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 4, 12) },
   {
     id: 'e3',
     title: 'Car Insurance',
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import Card from '../UI/Card';
 import ExpensesList from './ExpensesList';
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesChart from './ExpensesChart';
 import './Expenses.css';
 
 const Expenses = (props) => {
@@ -23,6 +24,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onFilterChangeHandler={filterChangeHandler}
         />
+        <ExpensesChart expenses={filteredExpenses} />
         <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
