e3fccb8ad51676e0c296e3b63156d0a1f1e7dd1b
elnur004 <lnur004@gmail.com>
2021-04-29 04:49:38 +0400

    update

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -23,7 +23,7 @@ const ExpensesForm = (props) => {
 
     const expenseData = {
       title: enteredTitle,
-      amount: enteredAmount,
+      amount: +enteredAmount,
       date: new Date(enteredDate),
     };
 
